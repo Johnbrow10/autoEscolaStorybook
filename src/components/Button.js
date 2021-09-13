5c82208bbea6589223a9c5ce3494df5735587346
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-09-13 19:55:47 -0300

    Criando componente de botão que tera uso global

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 1rem;
+  font-weight: 12px 36px;
+  text-transform: uppercase;
+  padding: 12px 36px;
+  cursor: pointer;
+  background-color: ${(props) => props.theme.colorYellow};
+  border: 2px solid #ffc107;
+  color: #212121;
+`;
+
+Button.defaultProps = {
+  type: "button",
+  children: undefined,
+};
+
+Button.propTypes = {
+  type: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Button;
